52f5a7baa6001f116442ae19f4ce44c448fa4fbf
jobkarani084j@gmail.com <jobkarani084j@gmail.com>
2022-08-29 15:51:20 +0300

    creates transactionsautomapper class

---
+++ Payment Simulation/Profiles/TransactionsAutoMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Payment_Simulation.Models;
+using Payment_Simulation.Services;
+using System.Transactions;
+
+namespace Payment_Simulation.Profiles
+{
+    public class TransactionsAutoMapper: Profile
+    {
+        public TransactionsAutoMapper()
+        {
+            CreateMap<Transactions, PaymentOrderDTO>().ReverseMap();
+            CreateMap<Recipient, PaymentOrderDTO>().ReverseMap();
+            CreateMap<Remitter, PaymentOrderDTO>().ReverseMap(); 
+        }
+    }
+}
